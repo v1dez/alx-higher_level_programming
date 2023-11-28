a2ddecaeb7053dfa6bd86a1c2fa11e6a13f94d77
Justin <orangijdavis427@gmail.com>
2023-11-28 06:34:13 -0800

    100o

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for char in range(97, 123, -1):
-	if char % 2 == 0:
-		print('{:c}'.format(char - 32), end='')
-	print('{:c}'.format(char), end='')
+for c in range(122, 96, -1):
+    if (c % 2 != 0):
+        c -= 32
+    print("{}".format(chr(c)), end="")
